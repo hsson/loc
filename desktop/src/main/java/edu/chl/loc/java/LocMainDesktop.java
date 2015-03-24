936156f49d7d046bd789666853b3e5ae757e6b81
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-03-24 10:22:00 +0100

    Updates game windows size and title

--- desktop/src/main/java/edu/chl/loc/java/LocMainDesktop.java
+++ desktop/src/main/java/edu/chl/loc/java/LocMainDesktop.java
@@ -8,6 +8,9 @@ import edu.chl.loc.core.LocMain;
 public class LocMainDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Legend of Chalmers";
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new LocMain(), config);
 	}
 }
