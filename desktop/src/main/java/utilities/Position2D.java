8b176f1c037b919eaba7fd145f3ef000fdf6412f
Maxim Goretskyy <13maxim13@live.se>
2015-04-03 23:24:33 +0200

    Created utilities package with a Position2D class, representing 2d-plane

---
+++ desktop/src/main/java/utilities/Position2D.java
@@ -0,0 +1,57 @@
+package utilities;
+
+/**
+ * Created by Maxim on 15-04-03.
+ * @author Maxim
+ *
+ * Class representing the position on a 2D-plane.
+ */
+
+
+public class Position2D {
+    private double xCoord;
+    private double yCoord;
+
+    public Position2D(double xCoord, double yCoord){
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+    }
+
+
+    public double getX(){
+        return xCoord;
+    }
+
+
+    public double getY(){
+        return yCoord;
+    }
+
+    /*adds given x- and y-coordinates to this Position2D.
+     * returns a new Position2D with the sum of x and y coordinates.
+     *
+     */
+    public Position2D add(double x, double y){
+        double tempX = this.getX() + x;
+        double tempY = this.getY() + y;
+        return new Position2D(tempX, tempY);
+    }
+
+    /*adds given the Position2D to this Position2D.
+     *
+     * throws IllegalArgumentException if given Position2D == null
+     * returns a new Position2D with the sum of x and y coordinates.
+     */
+    public Position2D add(Position2D position){
+        if(position == null){
+            throw new IllegalArgumentException("Can only add positions");
+        }
+        double tempX = this.xCoord + position.getX();
+        double tempY = this.yCoord + position.getY();
+        return new Position2D(tempX, tempY);
+    }
+
+    public String toString(){
+        return "Position of X-Coordinate is" + getX() + ", and Position of Y-Coordinate is " + getY();
+    }
+}
