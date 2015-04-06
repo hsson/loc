00c6987384f49dbfda66f6b6a24bd9503f2e00dd
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-04-06 21:55:59 +0200

    Updates map to use new layer representation

--- core/src/main/java/edu/chl/loc/map/AbstractMap.java
+++ core/src/main/java/edu/chl/loc/map/AbstractMap.java
@@ -6,12 +6,12 @@ import java.util.*;
  * A general representation of maps.
  *
  * @author Alexander Håkansson
- * @version 1.2.0
+ * @version 2.0.0
  * @since 2015-04-05
  */
 public abstract class AbstractMap {
 
-    private final Map<String, List<ITile>> mapTiles = new HashMap<String, List<ITile>>();
+    private final Map<ILayer, List<ITile>> mapTiles = new HashMap<ILayer, List<ITile>>();
 
     /**
      * Checks if the tile on the specified layer and coordinates is collidable
@@ -23,7 +23,7 @@ public abstract class AbstractMap {
      * @throws java.lang.IllegalArgumentException If the specified tile and layer doesn't exist
      * @return Returns true if the specified tile is collidable, false otherwise
      */
-    public boolean isCollidable(String layer, int x, int y) {
+    public boolean isCollidable(ILayer layer, int x, int y) {
         if (!mapTiles.containsKey(layer)) {
             throw new IllegalArgumentException("The layer specified does not exist");
         }
@@ -47,7 +47,7 @@ public abstract class AbstractMap {
      * @throws java.lang.IllegalArgumentException If the specified tile and layer doesn't exist
      * @return Returns the specified tile
      */
-    public ITile getTile(String layer, int x, int y) {
+    public ITile getTile(ILayer layer, int x, int y) {
         if (!mapTiles.containsKey(layer)) {
             throw new IllegalArgumentException("The layer \"" + layer + "\" does not exist");
         }
@@ -67,7 +67,7 @@ public abstract class AbstractMap {
      * @param layer The layer on which to place the tile
      * @param tile The tile to add to the map
      */
-    public void addTile(String layer, ITile tile) {
+    public void addTile(ILayer layer, ITile tile) {
         if (!mapTiles.containsKey(layer)) {
             mapTiles.put(layer, new ArrayList<ITile>());
             mapTiles.get(layer).add(tile);
@@ -82,7 +82,7 @@ public abstract class AbstractMap {
      * @param layer The layer to get the tiles from
      * @return A list of all tiles on the specified layer
      */
-    public List<ITile> getTilesFromLayer(String layer) {
+    public List<ITile> getTilesFromLayer(ILayer layer) {
         if (!mapTiles.containsKey(layer)) {
             throw new IllegalArgumentException("The specified layer does not exist");
         }
@@ -96,7 +96,7 @@ public abstract class AbstractMap {
      * @param layer The layer to check
      * @return Returns true if the layer exists, false otherwise
      */
-    public boolean layerExists(String layer) {
+    public boolean layerExists(ILayer layer) {
         return mapTiles.containsKey(layer);
     }
 
@@ -105,7 +105,7 @@ public abstract class AbstractMap {
      *
      * @return A set with all layers on the map
      */
-    public Set<String> getLayers() {
+    public Set<ILayer> getLayers() {
         return mapTiles.keySet();
     }
 
