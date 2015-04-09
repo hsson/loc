ee9dc1abbcd6318e0561c19efffa49473ee27680
Maxim Goretskyy <maxim@dhcp-188235.eduroam.chalmers.se>
2015-04-09 19:14:13 +0200

    added copy method and copy constructor, as well as itemID to be instancevariable

--- core/src/main/java/edu/chl/loc/Items/AbstractItem.java
+++ core/src/main/java/edu/chl/loc/Items/AbstractItem.java
@@ -8,15 +8,23 @@ import java.util.List;
  * @author Criticalstone
  * @version 1.0.0
  * @since 2015-04-07
+ * revised by Maxim Goretskyy
  */
-public abstract class AbstractItem {
+public abstract class AbstractItem{
 
     private ItemType type;
+    private int itemID;
 
-    public AbstractItem(ItemType type){
+    public AbstractItem(ItemType type, int itemID){
         this.type = type;
+        this.itemID = itemID;
     }
 
+    public AbstractItem(AbstractItem item){
+        this.type = item.getType();
+        this.itemID = item.getItemID();
+
+    }
     /**
      * @return The type that has been set to the item
      */
@@ -39,6 +47,26 @@ public abstract class AbstractItem {
      * @param type the type that should be added
      */
 
-    public abstract int getItemID();
+    /**
+     *
+     * @return itemID of this item
+     */
+    public int getItemID(){
+        return itemID;
+    }
 
+    /**
+     * Sets a given itemID for this item
+     * @param itemID itemID you want to set for this item
+     */
+
+    public void setItemID(int itemID){
+        this.itemID = itemID;
+    }
+
+    /**
+     * This method works as clone method, but clone method is worthless and should not be overriden or used, ever.
+     * @return A copy of abstractItem
+     */
+    public abstract AbstractItem copy();
 }
