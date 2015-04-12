027ba9521fa0022523301f951ed6696b42a3ba8b
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-04-12 22:08:21 +0200

    Implements a basic terminal based game showing of the game model

---
+++ core/src/main/java/edu/chl/loc/core/TerminalGame.java
@@ -0,0 +1,96 @@
+package edu.chl.loc.core;
+
+import edu.chl.loc.characters.Direction;
+import edu.chl.loc.characters.Player;
+import edu.chl.loc.map.*;
+import edu.chl.loc.utilities.Position2D;
+
+import java.util.Scanner;
+
+/**
+ * @author Alexander Håkansson
+ * @version 1.0.0
+ * @since 2015-04-12
+ */
+public class TerminalGame {
+
+    private final GameState gameState;
+
+    private final int boardSize = 10;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public TerminalGame() {
+        gameState = new GameState();
+
+        setupGame();
+        gameLoop();
+    }
+
+    public void setupGame() {
+        GameMap map = gameState.getGameMap();
+        ILayer groundLayer = new Layer("ground");
+        map.addLayer(new Layer("ground"));
+        for (int y = 0; y < boardSize; y++) {
+            for (int x = 0; x < boardSize; x++) {
+                map.addTile(groundLayer, new Tile(x, y, Math.random() < 0.05));
+            }
+        }
+        Position2D playerPos = gameState.getPlayer().getPosition();
+        map.getTile(groundLayer, (int) playerPos.getX(), (int) playerPos.getY()).setIsCollidable(false);
+    }
+
+    public void gameLoop() {
+        while (true) {
+            drawGame();
+            System.out.println("Move in any direction (u, d, l, r).");
+            System.out.print("> ");
+            Player p = gameState.getPlayer();
+            switch (scanner.nextLine().toCharArray()[0]) {
+                case 'd':
+                    // down is NORTH because of reversed y-axis
+                    p.setDirection(Direction.NORTH);
+                    break;
+                case 'u':
+                    // up is SOUTH because of reversed y-axis
+                    p.setDirection(Direction.SOUTH);
+                    break;
+                case 'l':
+                    p.setDirection(Direction.WEST);
+                    break;
+                case 'r':
+                    p.setDirection(Direction.EAST);
+                    break;
+            }
+
+            if (!gameState.getGameMap().getTile(new Layer("ground"),
+                    (int) p.getNextPosition().getX(),
+                    (int) p.getNextPosition().getY()).isCollidable()) {
+                p.move();
+            }
+        }
+    }
+
+    public void drawGame() {
+        GameMap map = gameState.getGameMap();
+        for (int y = 0; y < boardSize; y++) {
+            for (int x = 0; x < boardSize; x++) {
+                for (ILayer l : map.getLayers()) {
+                    ITile t = map.getTile(l, x, y);
+                    if (gameState.getPlayer().getPosition().equals(new Position2D(x, y))) {
+                        System.out.print("p");
+                    } else if (t.isCollidable()) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TerminalGame();
+    }
+}
