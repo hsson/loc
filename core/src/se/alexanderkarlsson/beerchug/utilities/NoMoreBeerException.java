d74c04b6af1d6a572ee099fffa4e93912d4b7a19
Sweshiman <alex94@comhem.se>
2015-04-17 18:00:43 +0200

    Added an exception for when you run out of beer

---
+++ core/src/se/alexanderkarlsson/beerchug/utilities/NoMoreBeerException.java
@@ -0,0 +1,11 @@
+package se.alexanderkarlsson.beerchug.utilities;
+
+/**
+ * Exception which is thrown when you try to drink beer, but can't
+ * find any beer. A very sad exception indeed.
+ * @author Alexander Karlsson
+ * @version 1.0
+ */
+public class NoMoreBeerException extends RuntimeException {
+
+}
