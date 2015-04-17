6b5bed019c7f708f9769ff72a9a840b84d142b8d
Sweshiman <alex94@comhem.se>
2015-04-17 18:01:25 +0200

    Added enum for directions that a bottle can be shaked

---
+++ core/src/se/alexanderkarlsson/beerchug/utilities/ShakeDirection.java
@@ -0,0 +1,10 @@
+package se.alexanderkarlsson.beerchug.utilities;
+
+/**
+ * Enum for specifying a direction which a bottle can be shaked
+ * @author Alexander Karlsson
+ * @version 0.1
+ */
+public enum ShakeDirection {
+    RIGHT, LEFT
+}
