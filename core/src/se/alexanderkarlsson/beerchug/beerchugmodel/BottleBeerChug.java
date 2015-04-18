6e653558828ba01c55a66530e5bbbc9cc6e92771
Sweshiman <alex94@comhem.se>
2015-04-18 13:54:38 +0200

    Finished basic functions of BottleBeerChug

--- core/src/se/alexanderkarlsson/beerchug/beerchugmodel/BottleBeerChug.java
+++ core/src/se/alexanderkarlsson/beerchug/beerchugmodel/BottleBeerChug.java
@@ -1,54 +1,79 @@
 package se.alexanderkarlsson.beerchug.beerchugmodel;
 
 import com.badlogic.gdx.utils.TimeUtils;
-import se.alexanderkarlsson.beerchug.utilities.NoMoreBeerException;
 import se.alexanderkarlsson.beerchug.utilities.ShakeDirection;
-import se.alexanderkarlsson.beerchug.utilities.SquirtException;
 
 /**
  * Beerchug model for bottle chugs
  * @author Alexander Karlsson
- * @version 0.1
+ * @version 1.0
  */
 public class BottleBeerChug {
     private int centilitersRemaining;
     private ShakeDirection lastShake;
-    private boolean chugStarted;
     private long timeStarted;
+    private long timeFinished;
+    private boolean finished;
+    private boolean squirted;
 
     public BottleBeerChug(){
         centilitersRemaining = 33;
         lastShake = null;
-        timeStarted = TimeUtils.nanoTime();
-        chugStarted = false;
+        timeStarted = TimeUtils.nanoTime()+3000000000l;//Added time for a three second countdown
+        finished = false;
     }
 
     /**
-     * Starts a chug
+     * Ends a chug
      */
-    public void startChug(){
-        chugStarted = true;
-        lastShake = ShakeDirection.LEFT;
+    public void endChug(){
+        if(centilitersRemaining != 0){
+            squirted = true;
+        }else if(!squirted){
+            finished = true;
+            timeFinished = TimeUtils.nanoTime();
+        }
     }
 
     /**
      * Shakes the bottle during a chug and drinks if done properly,
      * otherwise throws exception
      * @param shakeDirection The direction to shake the beer
-     * @throws NoMoreBeerException If you try to drink from an empty
-     * bottle an exception is thrown
-     * @throws SquirtException If you shake the same direction twice
-     * an exception is thrown
      */
-    public void shake(ShakeDirection shakeDirection) throws NoMoreBeerException, SquirtException{
-        if(shakeDirection == lastShake){
-            throw new SquirtException();
-        }else if(centilitersRemaining <= 0){
-            throw new NoMoreBeerException();
-        }else{
+    public void shake(ShakeDirection shakeDirection){
+        if(timeElapsed()<0 || lastShake == shakeDirection) {
+            squirted = true;
+        }else if(!squirted){
             lastShake = shakeDirection;
             centilitersRemaining--;
         }
 
+    }
+
+    public boolean isSquirted(){
+        return squirted;
+    }
+
+    /**
+     * Tells you if the chug is finished
+     * @return True if the chug is finished, false
+     * otherwise
+     */
+    public boolean isFinished(){
+        return finished;
+    }
+
+    /**
+     * Returns how long the current chug has been going on
+     * or the final time of the chug in nanoseconds
+     * @return The time elapsed if the chug is still ongoing
+     * or the final time if the chug is finished
+     */
+    public long timeElapsed(){
+        if(isFinished()){
+            return timeFinished-timeStarted;
+        }else{
+            return TimeUtils.nanoTime()- timeStarted;
+        }
     }
 }
