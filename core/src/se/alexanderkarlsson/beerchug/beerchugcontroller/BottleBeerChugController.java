8e6ef912925aedf6f079905c83394a84875ad262
Sweshiman <alex94@comhem.se>
2015-04-19 11:01:27 +0200

    Refactored code inside the controller so the key listening is handled internally

--- core/src/se/alexanderkarlsson/beerchug/beerchugcontroller/BottleBeerChugController.java
+++ core/src/se/alexanderkarlsson/beerchug/beerchugcontroller/BottleBeerChugController.java
@@ -1,5 +1,6 @@
 package se.alexanderkarlsson.beerchug.beerchugcontroller;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import se.alexanderkarlsson.beerchug.beerchugmodel.BottleBeerChug;
 import se.alexanderkarlsson.beerchug.utilities.ShakeDirection;
@@ -7,7 +8,7 @@ import se.alexanderkarlsson.beerchug.utilities.ShakeDirection;
 /**
  * Controller to be used with the BottleBeerChug class
  * @author Alexander Karlsson
- * @version 1.0
+ * @version 1.1
  */
 public class BottleBeerChugController {
     private BottleBeerChug model;
@@ -16,21 +17,20 @@ public class BottleBeerChugController {
         this.model = model;
     }
 
-    public void keyPressed(int keyPressed){
-        switch(keyPressed){
-            case Input.Keys.SPACE:
-                if(!model.isFirstShakeDone()) {
-                    model.firstShake();
-                }else{
-                    model.endChug();
-                }
-                break;
-            case Input.Keys.LEFT:
-                model.shake(ShakeDirection.LEFT);
-                break;
-            case Input.Keys.RIGHT:
-                model.shake(ShakeDirection.RIGHT);
-                break;
+    /**
+     * Checks if any key has been pushed and updates the model accordingly
+     */
+    public void update(){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            if(!model.isFirstShakeDone()) {
+                model.firstShake();
+            }else{
+                model.endChug();
+            }
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+            model.shake(ShakeDirection.LEFT);
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+            model.shake(ShakeDirection.RIGHT);
         }
     }
 }
