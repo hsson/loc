a93c8094a1075a036e084dc6c8b939f62dfa47e4
Maxim Goretskyy <13maxim13@live.se>
2015-04-22 16:32:21 +0200

    Added methods to generate gender and name

--- core/src/main/java/edu/chl/loc/characters/utilities/CharacterUtilities.java
+++ core/src/main/java/edu/chl/loc/characters/utilities/CharacterUtilities.java
@@ -14,9 +14,33 @@ public class CharacterUtilities {
     private static String[] femaleNames ={"Maria", "Anna", "Margareta", "Elisabeth", "Eva", "Kristina", "Birgitta",
                              "Karin", "Marie", "Ingrid", "Christina", "Linnea", "Sofia", "Kerstin", "Helena"};
 
+    private static Gender[] genderArray = {Gender.FEMALE, Gender.MALE};
+
     private static Random randomGen = new Random();
 
-    public static String generateName(){
-        return "hej";
+    public static Gender generateGender(){
+        int randomInt = randomGen.nextInt(genderArray.length);
+        return genderArray[randomInt];
     }
+
+    public static String generateName(Gender gender){
+        int randomInt = 0;
+        switch(gender){
+            case MALE:
+                randomInt = randomGen.nextInt(maleNames.length);
+                return maleNames[randomInt];
+            case FEMALE:
+                randomInt = randomGen.nextInt(femaleNames.length);
+                return femaleNames[randomInt];
+
+            default:
+                double probability = Math.random();
+                if(probability < 0.5){
+                    return maleNames[randomGen.nextInt(maleNames.length)];
+                }
+                return femaleNames[randomGen.nextInt(femaleNames.length)];
+        }
+    }
+
+    
 }
