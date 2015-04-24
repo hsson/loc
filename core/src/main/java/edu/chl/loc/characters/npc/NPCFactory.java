7822b4f1d56cfff6ed0cbd02c986fe6c51a8a466
Sweshiman <alex94@comhem.se>
2015-04-24 17:42:26 +0200

    Updated NPCFactory with new values and refactored the structure of the creation of NPCs

--- core/src/main/java/edu/chl/loc/characters/npc/NPCFactory.java
+++ core/src/main/java/edu/chl/loc/characters/npc/NPCFactory.java
@@ -4,49 +4,140 @@ import edu.chl.loc.characters.utilities.CharacterUtilities;
 import edu.chl.loc.characters.utilities.Direction;
 import edu.chl.loc.characters.utilities.Gender;
 import edu.chl.loc.items.Inventory;
+import edu.chl.loc.minigames.IMinigame;
 import edu.chl.loc.utilities.Position2D;
 
 /**
  * Factory class used for creating NPCs
  * @author Alexander Karlsson
- * @version 0.1
+ * @version 1.0
  */
 public class NPCFactory {
+    private static String name = null;
+    private static Gender gender = null;
+    private static Direction direction = null;
+    private static Inventory inventory = null;
+    private static IMinigame minigame = null;
+
     /**
-     * Creates an AbstractNPC with a given position, standard direction (north),
-     * empty inventory and random other values
-     * @return A new AbstractNPC object with given values and randomized other values
+     * Sets the name of the NPC currently being built
+     * @param name The name of the NPC to create
      */
-    public static AbstractNPC createNPC(Position2D position){
-        Gender gender = CharacterUtilities.generateGender();
-        String name = CharacterUtilities.generateName(gender);
-        AbstractNPC npc = new AbstractNPC(position, Direction.NORTH, name, new Inventory(), gender);
-        return npc;
+    public static void setName(String name){
+        NPCFactory.name = name;
     }
 
     /**
-     * Creates an AbstractNPC with given values and randomizes name and gender
-     * @param position The position of the AbstractNPC
-     * @param direction The direction of the AbstractNPC
-     * @param inventory The NPCs inventory
-     * @return An AbstractNPC with given values and randomized name and gender
+     * Sets the gender of the NPC currently being built
+     * @param gender The gender of the NPC to create
      */
-    public static AbstractNPC createNPC(Position2D position, Direction direction, Inventory inventory){
-        Gender gender = CharacterUtilities.generateGender();
-        String name = CharacterUtilities.generateName(gender);
-        return new AbstractNPC(position,direction,name,inventory,gender);
+    public static void setGender(Gender gender){
+        NPCFactory.gender = gender;
     }
 
     /**
-     * Creates and returns an AbstractNPC with all given values
-     * @param position The position for the AbstractNPC
-     * @param direction The NPCs direction
-     * @param name The NPCs name
-     * @param inventory The NPCs inventory
-     * @param gender The gender of the AbstractNPC
-     * @return A new
+     * Sets the direction of the NPC currently being built
+     * @param direction The direction of the NPC to create
      */
-    public static AbstractNPC createNPC(Position2D position, Direction direction, String name, Inventory inventory, Gender gender){
-        return new AbstractNPC(position, direction, name, inventory, gender);
+    public static void setDirection(Direction direction){
+        NPCFactory.direction = direction;
+    }
+
+    /**
+     * Sets the inventory of the NPC being built
+     * @param inventory The inventory of the NPC being built
+     * @throws CannotSetThisValueException If the current build already has a minigame set a inventory cant be set
+     */
+    public static void setInventory(Inventory inventory) throws CannotSetThisValueException{
+        if(NPCFactory.isMinigameSet()){
+            throw new CannotSetThisValueException("Cannot set inventory when minigame is set");
+        }else{
+            NPCFactory.inventory = inventory;
+        }
+    }
+
+    /**
+     * Sets the minigame of the NPC being built
+     * @param minigame The minigame of the NPC being built
+     * @throws CannotSetThisValueException If the current build already has an inventory set a minigame cant be set
+     */
+    public static void setMinigame(IMinigame minigame) throws CannotSetThisValueException{
+        if(NPCFactory.isInventorySet()){
+            throw new CannotSetThisValueException("Cannot set minigame when inventory is set");
+        }else {
+            NPCFactory.minigame = minigame;
+        }
+    }
+
+    /**
+     * Checks if the current build has an inventory set
+     * @return True if an inventory is set, false otherwise
+     */
+    public static boolean isInventorySet(){
+        return NPCFactory.inventory != null;
+    }
+
+    /**
+     * Checks if the current build has a minigame set
+     * @return True if a minigame is set, false otherwise
+     */
+    public static boolean isMinigameSet(){
+        return NPCFactory.minigame != null;
+    }
+
+    /**
+     * Resets all the values of the current build
+     */
+    public static void reset(){
+        NPCFactory.name = null;
+        NPCFactory.gender = null;
+        NPCFactory.direction = null;
+        NPCFactory.inventory = null;
+        NPCFactory.minigame = null;
+    }
+
+    /**
+     * Builds and returns the current build using the values from the set methods
+     * @param position The position the new NPC should have
+     * @return An NPC with values from set methods and given position
+     */
+    public static AbstractNPC build(Position2D position){
+        Gender gender;
+        String name;
+        Direction direction;
+        Inventory inventory;
+        IMinigame minigame;
+
+        if(NPCFactory.gender == null){
+            gender = CharacterUtilities.generateGender();
+        }else{
+            gender = NPCFactory.gender;
+        }
+
+        if(NPCFactory.name == null){
+            name = CharacterUtilities.generateName(NPCFactory.gender);
+        }else{
+            name = NPCFactory.name;
+        }
+
+        if(NPCFactory.direction == null){
+            direction = Direction.NORTH;//Standard direction is north
+        }else{
+            direction = NPCFactory.direction;
+        }
+
+        if(isInventorySet()){
+            inventory = NPCFactory.inventory;
+            NPCFactory.reset();
+            return new ItemNPC(position,direction,name,gender);
+        }
+
+        if(isMinigameSet()){
+            minigame = NPCFactory.minigame;
+            NPCFactory.reset();
+            return new MinigameNPC(position,direction,name,gender,minigame);
+        }
+
+        return new StandardNPC(position,direction,name,gender);
     }
 }
