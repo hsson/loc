a3f1659a96e45097da343deb1e7ee1e6efe9d062
Maxim Goretskyy <13maxim13@live.se>
2015-04-24 16:25:01 +0200

    Added StandardNPC class

---
+++ core/src/main/java/edu/chl/loc/characters/npc/StandardNPC.java
@@ -0,0 +1,22 @@
+package edu.chl.loc.characters.npc;
+
+import edu.chl.loc.characters.utilities.Direction;
+import edu.chl.loc.characters.utilities.Gender;
+import edu.chl.loc.items.Inventory;
+import edu.chl.loc.utilities.Position2D;
+
+/**
+ * Created by maxim on 15-04-24.
+ * @author Maxim Goretskyy
+ */
+public class StandardNPC extends AbstractNPC {
+
+    protected StandardNPC(Position2D pos, Direction dir, String name, Gender gender) {
+        super(pos, dir, name, gender);
+    }
+
+    @Override
+    protected void doAction() {
+
+    }
+}
