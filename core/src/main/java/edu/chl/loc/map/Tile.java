ce2fc6a63dbd112e5e4617987a2ed062c466e3a2
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-04-24 15:57:00 +0200

    Updates Tile to use AbstractTile

--- core/src/main/java/edu/chl/loc/map/Tile.java
+++ core/src/main/java/edu/chl/loc/map/Tile.java
@@ -11,10 +11,7 @@ import edu.chl.loc.utilities.Position2D;
  * @version 2.0.0
  * @since 2015-04-07
  */
-public class Tile implements ITile{
-
-    private boolean isCollidable = false;
-    private Position2D pos;
+public class Tile extends AbstractTile{
 
     /**
      * Creates a new tile with the specified coordinates and sets isCollidable
@@ -24,8 +21,8 @@ public class Tile implements ITile{
      * @param isCollidable if the tile is collidable or not
      */
     public Tile(Position2D position, boolean isCollidable){
-        this(position);
-        this.isCollidable = isCollidable;
+        super(position);
+        this.setIsCollidable(isCollidable);
     }
 
     /**
@@ -35,23 +32,7 @@ public class Tile implements ITile{
      * @param position The position of the Tile
      */
     public Tile(Position2D position){
-        pos = position.copy();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public boolean isCollidable() {
-        return this.isCollidable;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public void setIsCollidable(boolean isCollidable) {
-        this.isCollidable = isCollidable;
+        super(position);
     }
 
     /**
@@ -60,14 +41,6 @@ public class Tile implements ITile{
     @Override
     public boolean hasItem() {
         return false;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    @Override
-    public Position2D getPosition() {
-        return this.pos;
     }
 
     @Override
@@ -85,6 +58,6 @@ public class Tile implements ITile{
         }
 
         Tile other = (Tile) o;
-        return this.pos.getX() == other.pos.getX() && this.pos.getY() == other.pos.getY();
+        return this.getPosition().equals(other.getPosition());
     }
 }
