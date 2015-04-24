b9920a6f1a5871a69ad3ddd51c53fe9089ba08de
Maxim Goretskyy <13maxim13@live.se>
2015-04-24 16:29:13 +0200

    Refactored gamemap,changed NPC to AbstractNPC

--- core/src/main/java/edu/chl/loc/map/GameMap.java
+++ core/src/main/java/edu/chl/loc/map/GameMap.java
@@ -1,6 +1,6 @@
 package edu.chl.loc.map;
 
-import edu.chl.loc.characters.NPC;
+import edu.chl.loc.characters.npc.AbstractNPC;
 import edu.chl.loc.utilities.Position2D;
 
 import java.util.ArrayList;
@@ -13,18 +13,18 @@ import java.util.List;
  */
 public class GameMap extends AbstractMap {
 
-    private final List<NPC> npcList =  new ArrayList<NPC>();
+    private final List<AbstractNPC> npcList =  new ArrayList<AbstractNPC>();
 
     public GameMap() {
         super();
     }
 
     /**
-     * Add an NPC to the map if it hasn't already been added.
+     * Add an AbstractNPC to the map if it hasn't already been added.
      *
-     * @param npc The NPC to add.
+     * @param npc The AbstractNPC to add.
      */
-    public void addNPC(final NPC npc) {
+    public void addNPC(final AbstractNPC npc) {
         if (!npcList.contains(npc)) {
             npcList.add(npc);
         }
@@ -35,53 +35,53 @@ public class GameMap extends AbstractMap {
      *
      * @return A list of all NPCs on the map
      */
-    public List<NPC> getAllNPCs() {
+    public List<AbstractNPC> getAllNPCs() {
         return npcList;
     }
 
     /**
-     * Get the NPC on the specified position. This only gets the first NPC
-     * and if there happen to be more than one NPC on the same position
-     * this will only get one. If no NPC can be found on the given position,
+     * Get the AbstractNPC on the specified position. This only gets the first AbstractNPC
+     * and if there happen to be more than one AbstractNPC on the same position
+     * this will only get one. If no AbstractNPC can be found on the given position,
      * and exception will be thrown.
      *
-     * @param pos The position of the NPC to get
-     * @return The NPC at the specified position
-     * @throws java.lang.IllegalArgumentException If there is not NPC at the given position
+     * @param pos The position of the AbstractNPC to get
+     * @return The AbstractNPC at the specified position
+     * @throws java.lang.IllegalArgumentException If there is not AbstractNPC at the given position
      */
-    public NPC getNPCAtPosition(final Position2D pos) {
-        for (NPC npc : npcList) {
+    public AbstractNPC getNPCAtPosition(final Position2D pos) {
+        for (AbstractNPC npc : npcList) {
             if (npc.getPosition().equals(pos)) {
                 return npc;
             }
         }
 
-        throw new IllegalArgumentException("No NPC at the specified position");
+        throw new IllegalArgumentException("No AbstractNPC at the specified position");
     }
 
     /**
-     * Get the NPC on the specified position. This only gets the first NPC
-     * and if there happen to be more than one NPC on the same position
-     * this will only get one. If no NPC can be found on the given position,
+     * Get the AbstractNPC on the specified position. This only gets the first AbstractNPC
+     * and if there happen to be more than one AbstractNPC on the same position
+     * this will only get one. If no AbstractNPC can be found on the given position,
      * and exception will be thrown.
      *
-     * @param x The position in the x-axis of the NPC to get
-     * @param y The position in the y-axis of the NPC to get
-     * @return The NPC at the specified position
-     * @throws java.lang.IllegalArgumentException If there is not NPC at the given position
+     * @param x The position in the x-axis of the AbstractNPC to get
+     * @param y The position in the y-axis of the AbstractNPC to get
+     * @return The AbstractNPC at the specified position
+     * @throws java.lang.IllegalArgumentException If there is not AbstractNPC at the given position
      */
-    public NPC getNPCAtPosition(int x, int y) {
+    public AbstractNPC getNPCAtPosition(int x, int y) {
         return getNPCAtPosition(new Position2D(x, y));
     }
 
     /**
-     * Check if there is any NPC at the given position on the map.
+     * Check if there is any AbstractNPC at the given position on the map.
      *
-     * @param pos The position to check for an NPC
-     * @return True if there is any NPC at the given position, false otherwise
+     * @param pos The position to check for an AbstractNPC
+     * @return True if there is any AbstractNPC at the given position, false otherwise
      */
     public boolean npcExisitsAtPosition(final Position2D pos) {
-        for (NPC npc : npcList) {
+        for (AbstractNPC npc : npcList) {
             if (npc.getPosition().equals(pos)) {
                 return true;
             }
@@ -91,11 +91,11 @@ public class GameMap extends AbstractMap {
     }
 
     /**
-     * Check if there is any NPC at the given position on the map.
+     * Check if there is any AbstractNPC at the given position on the map.
      *
-     * @param x The position in the x-axis to check for an NPC
-     * @param y The position in the y-axis to check for an NPC
-     * @return True if there is any NPC at the given position, false otherwise
+     * @param x The position in the x-axis to check for an AbstractNPC
+     * @param y The position in the y-axis to check for an AbstractNPC
+     * @return True if there is any AbstractNPC at the given position, false otherwise
      */
     public boolean npcExistsAtPosition(int x, int y) {
         return npcExisitsAtPosition(new Position2D(x, y));
