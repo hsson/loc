e3560883f5a3d0c12003128c3327e4cb6379d05c
Maxim Goretskyy <13maxim13@live.se>
2015-04-28 16:26:24 +0200

    Moved characterview to another package for characters inside view

--- core/src/main/java/edu/chl/loc/view/characters/CharacterView.java
+++ core/src/main/java/edu/chl/loc/view/characters/CharacterView.java
@@ -1,14 +1,16 @@
-package edu.chl.loc.view.core;
+package edu.chl.loc.view.characters;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import edu.chl.loc.models.characters.AbstractCharacter;
+import edu.chl.loc.view.core.IGameView;
+import edu.chl.loc.view.core.IView;
 
 /**
  * Created by maxim on 15-04-28.
  * @author Maxim Goretskyy
  */
-public class CharacterView implements IView{
+public class CharacterView implements IView {
     private AbstractCharacter absCharacter;
     private Texture charTexture;
     private SpriteBatch spriteBatch;
