46344d27d7650f9dd6ea12d6b7e9c99f8fb986d3
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-04-28 14:42:11 +0200

    Adds a package for view module and basic game view interface

---
+++ core/src/main/java/edu/chl/loc/view/core/IGameView.java
@@ -0,0 +1,12 @@
+package edu.chl.loc.view.core;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * @author Alexander Håkansson
+ * @version 1.0.0
+ * @since 2015-04-28
+ */
+public interface IGameView {
+    public SpriteBatch getSpriteBatch();
+}
