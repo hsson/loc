55fa9cb4c21e639627d7e6e923ad4eebff78a0e0
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-04-30 01:33:29 +0200

    Updates LocMain to extend the Game class

--- core/src/main/java/edu/chl/loc/LocMain.java
+++ core/src/main/java/edu/chl/loc/LocMain.java
@@ -1,39 +1,57 @@
 package edu.chl.loc;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import edu.chl.loc.controller.GameController;
+import edu.chl.loc.models.core.GameModel;
+import edu.chl.loc.view.core.GameView;
 
-public class LocMain implements ApplicationListener {
-	SpriteBatch batch;
-	float elapsed;
+public class LocMain extends Game {
+	private float elapsed;
+    private float delta;
+
+    private GameModel model;
+    private GameController controller;
+    private GameView view;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-	}
+        model = new GameModel();
+        controller = new GameController(model);
+        view = new GameView(model, null, null); //TODO: GameView should not take last two input arguments
+
+        setScreen(view);
+    }
 
 	@Override
 	public void resize (int width, int height) {
-	}
+	    getScreen().resize(width, height);
+    }
 
 	@Override
 	public void render () {
-		elapsed += Gdx.graphics.getDeltaTime();
+        delta = Gdx.graphics.getDeltaTime();
+		elapsed += delta;
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+        getScreen().render(delta);
 	}
 
 	@Override
 	public void pause () {
+        getScreen().pause();
 	}
 
 	@Override
 	public void resume () {
+        getScreen().resume();
 	}
 
 	@Override
 	public void dispose () {
+        getScreen().dispose();
 	}
 }
