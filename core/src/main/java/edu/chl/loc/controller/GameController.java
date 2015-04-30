75c90babc795cd22b5735550d11f3cc08266b3f9
Maxim Goretskyy <13maxim13@live.se>
2015-04-30 09:29:10 +0200

    Refactored the choice of movement to a method

--- core/src/main/java/edu/chl/loc/controller/GameController.java
+++ core/src/main/java/edu/chl/loc/controller/GameController.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.InputProcessor;
 import edu.chl.loc.models.characters.Player;
 import edu.chl.loc.models.characters.utilities.Direction;
 import edu.chl.loc.models.core.GameModel;
+import edu.chl.loc.models.map.GameMap;
+import edu.chl.loc.models.map.Layer;
+import edu.chl.loc.models.utilities.Position2D;
 
 /**
  * @author Alexander Håkansson
@@ -16,37 +19,31 @@ public class GameController implements InputProcessor {
 
     private final GameModel model;
     private Player player;
+    private GameMap gameMap; //todo make gamemap static inside gamemodel?
 
+    /**
+     *
+     * @param model The model you want to control
+     */
     public GameController(GameModel model) {
         this.model = model;
         this.player = model.getPlayer();
+        this.gameMap = model.getGameMap();
 
     }
 
     @Override
-    public boolean keyDown(int keycode) {
+    public boolean keyDown(int keycode) {// assuming smooth movement will be here?
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-        switch(keycode){
-            case  Input.Keys.LEFT:
-                player.setDirection(Direction.WEST);
-                break;
-
-            case  Input.Keys.RIGHT:
-                player.setDirection(Direction.EAST);
-                break;
-
-            case  Input.Keys.UP:
-                player.setDirection(Direction.NORTH);
-                break;
-
-            case  Input.Keys.DOWN:
-                player.setDirection(Direction.SOUTH);
-                break;
-        }
+        Position2D nextPos = player.getNextPosition();
+        //if(gameMap.isCollidable(new Layer("NonexistableLayerJustFillingOut"), nextPos))
+        //todo choose a name for players layer where he moves
+        //get tile from next position and see if its collidable with nextPos
+        chooseMovement(keycode);
         player.move();
         updateViews();
         return false;
@@ -82,8 +79,29 @@ public class GameController implements InputProcessor {
         return false;
     }
 
+    public void chooseMovement(int keycode){
+        switch(keycode){
+            case  Input.Keys.LEFT:
+                player.setDirection(Direction.WEST);
+                break;
+
+            case  Input.Keys.RIGHT:
+                player.setDirection(Direction.EAST);
+                break;
+
+            case  Input.Keys.UP:
+                player.setDirection(Direction.NORTH);
+                break;
+
+            case  Input.Keys.DOWN:
+                player.setDirection(Direction.SOUTH);
+                break;
+        }
+    }
     //Todo update views in this method
     public void updateViews(){
 
     }
+
+
 }
