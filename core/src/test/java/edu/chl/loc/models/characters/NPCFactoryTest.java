cc50359eaad0c7b026feaf9db4a72f97379633d7
Maxim Goretskyy <13maxim13@live.se>
2015-04-30 19:32:04 +0200

    added more tests to npcfactorytest

--- core/src/test/java/edu/chl/loc/models/characters/NPCFactoryTest.java
+++ core/src/test/java/edu/chl/loc/models/characters/NPCFactoryTest.java
@@ -5,7 +5,9 @@ import edu.chl.loc.models.items.Inventory;
 import edu.chl.loc.models.minigames.IMinigame;
 import edu.chl.loc.models.utilities.Position2D;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by maxim on 15-04-30.
@@ -19,7 +21,6 @@ public class NPCFactoryTest {
     public void testBuildItemNPC() {
         NPCFactory.setInventory(new Inventory());
         AbstractNPC firstNPC = NPCFactory.build(new Position2D(4,4));
-        NPCFactory.reset();
         Assert.assertEquals("The created NPC should be an ItemNPC", firstNPC.getClass(), ItemNPC.class);
 
     }
@@ -33,17 +34,49 @@ public class NPCFactoryTest {
             }
         });
         AbstractNPC secondNPC = NPCFactory.build(new Position2D(4,4));
-        NPCFactory.reset();
         Assert.assertEquals("The created NPC should be a MinigameNPC", secondNPC.getClass(), MinigameNPC.class);
 
     }
     @Test
     public void testBuildStandardNPC() {
         AbstractNPC thirdNPC = NPCFactory.build(new Position2D(4,4));
-        NPCFactory.reset();
         Assert.assertEquals("The created NPC should be a StandardNPC", thirdNPC.getClass(), StandardNPC.class);
     }
 
+    @Test
+    public void testAddInvAndMinigame(){
+        NPCFactory.setInventory(new Inventory());
+        try {
 
+            NPCFactory.setMinigame(new IMinigame() {
+                @Override
+                public int hashCode() {
+                    return 9;
+                }
+            });
+        }catch(CannotSetThisValueException ex){
+            NPCFactory.reset();
+            Assert.assertEquals(ex.getMessage(), "Cannot set minigame when inventory is set");
+
+        }
+
+    }
+
+    @Test
+    public void testAddMinigameAndInv(){
+        NPCFactory.setMinigame(new IMinigame() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        try {
+            NPCFactory.setInventory(new Inventory());
+        }catch(CannotSetThisValueException ex){
+            NPCFactory.reset();
+            Assert.assertEquals(ex.getMessage(), "Cannot set inventory when minigame is set");
+        }
+
+    }
 
 }
