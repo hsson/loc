7172879c7fecad555d1ad37843f086753e6e263e
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-04 00:58:39 +0200

    Updates the GameMapView to handle NPC rendering

--- core/src/main/java/edu/chl/loc/view/map/GameMapView.java
+++ core/src/main/java/edu/chl/loc/view/map/GameMapView.java
@@ -1,18 +1,19 @@
 package edu.chl.loc.view.map;
 
+import com.badlogic.gdx.graphics.Texture;
 import edu.chl.loc.models.characters.npc.AbstractNPC;
 import edu.chl.loc.models.core.GameModel;
 import edu.chl.loc.view.characters.CharacterView;
+import edu.chl.loc.view.characters.NPCTextureFactory;
 import edu.chl.loc.view.core.GameView;
 import edu.chl.loc.view.core.IView;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
  * @author Alexander Håkansson
- * @version 0.1.0
+ * @version 0.2.0
  * @since 2015-04-30
  */
 public class GameMapView implements IView {
@@ -20,18 +21,34 @@ public class GameMapView implements IView {
     private final GameView gameView;
     private final GameModel gameModel;
 
-    private Set<CharacterView> npcViews = new HashSet<CharacterView>();
-    private List<AbstractNPC> npcModels;
+    private Set<CharacterView> npcViews;
 
     public GameMapView(final GameView gameView) {
         this.gameView = gameView;
         this.gameModel = gameView.getGameModel();
 
         // TODO: Create CharacterViews and ItemViews based on something so they can be rendered
+        createNPCViews();
+    }
+
+    /**
+     * Gets all NPC from the model and creates views for them.
+     */
+    private void createNPCViews() {
+        npcViews = new HashSet<CharacterView>();
+
+        for (AbstractNPC npc : gameModel.getGameMap().getAllNPCs()) {
+            Texture npcTexture = NPCTextureFactory.build(npc);
+            npcViews.add(new CharacterView(npc, npcTexture));
+        }
     }
 
     @Override
     public void render() {
 
+        // Render NPCs
+        for (CharacterView view : npcViews) {
+            view.render();
+        }
     }
 }
