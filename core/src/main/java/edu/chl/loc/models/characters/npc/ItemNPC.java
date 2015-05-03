fca2e75b2a5a8683c15b915f021bc2b675099ede
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-04 01:24:32 +0200

    Updates ItemNPC to give its items to the player on doAction()

--- core/src/main/java/edu/chl/loc/models/characters/npc/ItemNPC.java
+++ core/src/main/java/edu/chl/loc/models/characters/npc/ItemNPC.java
@@ -2,13 +2,15 @@ package edu.chl.loc.models.characters.npc;
 
 import edu.chl.loc.models.characters.utilities.Direction;
 import edu.chl.loc.models.characters.utilities.Gender;
+import edu.chl.loc.models.core.GameModel;
+import edu.chl.loc.models.items.AbstractItem;
 import edu.chl.loc.models.items.Inventory;
 import edu.chl.loc.models.utilities.Position2D;
 
 /**
  * Created by maxim on 15-04-24.
  * @author Maxim Goretskyy
- * Revised by Alexander Karlsson
+ * Revised by Alexander Karlsson, Alexander Håkansson
  */
 public class ItemNPC extends AbstractNPC{
     private Inventory inventory;
@@ -21,7 +23,11 @@ public class ItemNPC extends AbstractNPC{
 
     @Override
     protected void doAction() {
-        //Todo action to give an item
+
+        // Adds all of this NPC's items to the player
+        for (AbstractItem item : inventory.getItems()) {
+            GameModel.getPlayer().getInventory().addItem(item);
+        }
     }
 
     /**
