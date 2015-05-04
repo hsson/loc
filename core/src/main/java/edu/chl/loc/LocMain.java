a52f0f1163b3fbacfbddf1e841d1e5c09137f8ac
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-05-04 17:35:24 +0200

    Adds a method that creates all NPCs from the NPCs.loc and adds all of them to the gamemap

--- core/src/main/java/edu/chl/loc/LocMain.java
+++ core/src/main/java/edu/chl/loc/LocMain.java
@@ -4,8 +4,17 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL30;
 import edu.chl.loc.controller.GameController;
+import edu.chl.loc.models.characters.npc.NPCFactory;
+import edu.chl.loc.models.characters.utilities.Gender;
 import edu.chl.loc.models.core.GameModel;
+import edu.chl.loc.models.items.Inventory;
+import edu.chl.loc.models.map.GameMap;
+import edu.chl.loc.models.utilities.Position2D;
 import edu.chl.loc.view.core.GameView;
+import javafx.geometry.Pos;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Alexander Håkansson
@@ -58,4 +67,24 @@ public class LocMain extends Game {
 	public void dispose () {
         getScreen().dispose();
 	}
+
+    /**
+     * Creates all the NPCs from the textfile NPCs.loc and adds each
+     * of the to the gamemap
+     */
+    private void createNPCsFromFile(){
+        Position2D position;
+        List<List<String>> NPCList = FileUtilities.readFile("NPCs.loc");
+        for(List<String> NPCProperty: NPCList){
+            NPCFactory.setId(Integer.parseInt(NPCProperty.get(0)));
+            NPCFactory.setName(NPCProperty.get(1));
+            NPCFactory.setGender(Gender.valueOf(NPCProperty.get(2)));
+            position = new Position2D(Integer.parseInt(NPCProperty.get(3)),
+                                      Integer.parseInt(NPCProperty.get(4)));
+            //TODO: create Inventory with items that are specified
+            //TODO: check the position of the NPC before creating it
+            //TODO: call this method somewhere before the game is rendered
+            model.getGameMap().addNPC(NPCFactory.build(position));
+        }
+    }
 }
