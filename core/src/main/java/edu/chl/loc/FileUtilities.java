4d02ac24ffd2615cb8753b7b9ec251cc45578873
Maxim Goretskyy <13maxim13@live.se>
2015-05-05 15:16:05 +0200

    Modified FileUtilities to read in files without libgdx

--- core/src/main/java/edu/chl/loc/FileUtilities.java
+++ core/src/main/java/edu/chl/loc/FileUtilities.java
@@ -1,8 +1,10 @@
 package edu.chl.loc;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,31 +18,57 @@ public class FileUtilities {
 
     }
 
-
+    /**
+     *
+     * @param pathToFile the path you want to read from, relative to assest (i think and hope)
+     * @return List of a List with strings
+     */
     public static List<List<String>> readFile(String pathToFile){
 
         String[] temp = loadContent(pathToFile);
 
         List<List<String>> result = new ArrayList<List<String>>();
         for(int i = 0; i<temp.length; i++){
-            result.add(removeColons(temp[i]));
+            if(!temp[i].equals("")){ // ignores empty strings
+                result.add(removeColons(temp[i]));
+            }
+
 
         }
         return result;
     }
-    
+
     private static String[] loadContent(String pathToFile){
-        if(Gdx.files.internal(pathToFile).exists()){
-            FileHandle file = Gdx.files.internal(pathToFile);
-            String test = file.readString();
-            String[] result = test.split("\\n");
-            return result;
-        }
-        throw new NullPointerException("File does not exist");
+        String temp1 = load(pathToFile);
+
+        String temp2 = temp1.replaceAll("(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|(//.*)", ""); //ignores all
+                                                                                        //java-styled comments
+        String temp3 = temp2.replaceAll("^\\s+$", ""); // replaces all white lines with an empty string, like null
+        String[] result = temp3.split("\\n"); //Strip every row
+        return result;
 
     }
 
-    private static List<String> removeColons(String textString){
+    /**
+     *
+     * @param filename Filename you want to read
+     * @return String of the whole file.
+     */
+    private static String load(String filename){
+
+            File file = new File(filename);
+            try {
+                byte[] bytes = Files.readAllBytes(file.toPath());
+                return new String(bytes,"UTF-8");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+
+    }
+    private static List<String> removeColons(String textString){//also ignores whitelines
         String[] temp = textString.split(":");
         List<String> result = new ArrayList<String>();
         for(int i =0; i<temp.length;i++){ //copies to a list
@@ -55,11 +83,9 @@ public class FileUtilities {
         for(int i = 0; i<list.size(); i++){
             for(int j = 0; j<list.get(i).size(); j++){
                 System.out.println("Word is " +list.get(i).get(j));
-
             } System.out.println("XXXXXXX");
         }
+        System.out.println("Size of teh list is " + list.size());
     }
-
-
 
 }
