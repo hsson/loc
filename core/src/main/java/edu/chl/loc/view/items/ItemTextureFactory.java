6eee8a93426ad057d62eb54375aeb69939a440df
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-06 15:51:54 +0200

    Implements a basic factory for getting the correct texture for an item

---
+++ core/src/main/java/edu/chl/loc/view/items/ItemTextureFactory.java
@@ -0,0 +1,28 @@
+package edu.chl.loc.view.items;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import edu.chl.loc.models.items.AbstractItem;
+
+/**
+ * @author Alexander Håkansson
+ * @version 1.0.0
+ * @since 2015-05-06
+ */
+public class ItemTextureFactory {
+
+    private static final Texture DEFAULT_ITEM_TEXTURE = new Texture(Gdx.files.internal("items/pripps.png"));
+
+    private ItemTextureFactory() {
+        // No construction allowed
+    }
+
+    public static Texture build(AbstractItem item) {
+
+        if (item.getItemName().toLowerCase().equals("pripps blå")) {
+            return new Texture(Gdx.files.internal("items/pripps.png"));
+        }
+
+        return DEFAULT_ITEM_TEXTURE;
+    }
+}
