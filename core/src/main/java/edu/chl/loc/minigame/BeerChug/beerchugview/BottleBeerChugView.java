2cad5b0bbc0b3351d71e952b085830cb50b0c87f
Sweshiman <alex94@comhem.se>
2015-05-12 15:38:21 +0200

    Removed unused import

--- core/src/main/java/edu/chl/loc/minigame/BeerChug/beerchugview/BottleBeerChugView.java
+++ core/src/main/java/edu/chl/loc/minigame/BeerChug/beerchugview/BottleBeerChugView.java
@@ -1,7 +1,6 @@
 package edu.chl.loc.minigame.BeerChug.beerchugview;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
