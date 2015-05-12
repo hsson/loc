f408f0858e927ff49f25356af5ef8263077edb31
Sweshiman <alex94@comhem.se>
2015-05-12 13:42:52 +0200

    Adjusted main class to new controller

--- core/src/se/alexanderkarlsson/beerchug/BeerChug.java
+++ core/src/se/alexanderkarlsson/beerchug/BeerChug.java
@@ -2,17 +2,21 @@ package se.alexanderkarlsson.beerchug;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import se.alexanderkarlsson.beerchug.beerchugcontroller.BottleBeerChugController;
 import se.alexanderkarlsson.beerchug.beerchugmodel.BottleBeerChug;
 import se.alexanderkarlsson.beerchug.beerchugview.BottleBeerChugView;
 
 public class BeerChug extends Game {
 	private BottleBeerChug model;
+	private BottleBeerChugController controller;
 	private BottleBeerChugView view;
 
 	@Override
 	public void create() {
 		model = new BottleBeerChug();
 		view = new BottleBeerChugView(model);
+		controller = new BottleBeerChugController(model);
+		Gdx.input.setInputProcessor(controller);
 		this.setScreen(view);
 	}
 
