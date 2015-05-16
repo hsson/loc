b31dd60cda1edafd4b15bdd43bdbaa4898054f88
Sweshiman <alex94@comhem.se>
2015-05-16 23:04:49 +0200

    Added caps view class

---
+++ core/src/main/java/edu/chl/loc/minigame/caps/capsview/CapsGameView.java
@@ -0,0 +1,45 @@
+package edu.chl.loc.minigame.caps.capsview;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * View class for caps minigame
+ * @author Alexander Karlsson
+ * @version 1.0
+ */
+public class CapsGameView implements Screen {
+    @Override
+    public void render(float delta) {
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
