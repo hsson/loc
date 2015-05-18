42750639e4aa3a6172466dd1ef02958ebd5ca8a0
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-18 11:24:47 +0200

    Makes the drawing of the menu more dynamic

--- core/src/main/java/edu/chl/loc/view/menu/GameMenuView.java
+++ core/src/main/java/edu/chl/loc/view/menu/GameMenuView.java
@@ -1,14 +1,18 @@
 package edu.chl.loc.view.menu;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import edu.chl.loc.models.menu.GameMenu;
+import edu.chl.loc.models.menu.IMenuOption;
 import edu.chl.loc.view.core.GameView;
 import edu.chl.loc.view.core.IView;
 import edu.chl.loc.view.utilities.RenderUtilities;
+
+import java.util.List;
 
 /**
  * @author Alexander Håkansson
@@ -23,6 +27,8 @@ public class GameMenuView implements IView {
     private ShapeRenderer shapeRenderer;
     private BitmapFont font;
 
+    private int textYOffset = 64;
+
     public GameMenuView(GameMenu menu) {
         this.gameMenu = menu;
         shapeRenderer = new ShapeRenderer();
@@ -35,14 +41,28 @@ public class GameMenuView implements IView {
         Matrix4 oldProjMatrix = batch.getProjectionMatrix();
         batch.setProjectionMatrix(shapeRenderer.getProjectionMatrix());
         Viewport viewport = GameView.getViewport();
+        List<IMenuOption> options = gameMenu.getMenuOptions();
 
         float menuWidth = viewport.getWorldWidth() / 5;
-        float menuHeight = viewport.getWorldHeight() / 2;
+        float menuHeight = textYOffset * options.size();
 
         float xPos = (viewport.getWorldWidth() / 4) * 3;
         float yPos = (viewport.getWorldHeight() / 5) * 2;
 
+        float textX = xPos + 32;
+        float textY = yPos + menuHeight - 32;
+
         RenderUtilities.renderRectangle(xPos, yPos, menuWidth, menuHeight, shapeRenderer);
+
+        batch.begin();
+        font.setColor(Color.BLACK);
+
+        for (IMenuOption option : options) {
+            int index = options.indexOf(option);
+            font.draw(batch, option.getName(), textX, textY + textYOffset * index);
+        }
+        batch.end();
+
 
         batch.setProjectionMatrix(oldProjMatrix);
     }
