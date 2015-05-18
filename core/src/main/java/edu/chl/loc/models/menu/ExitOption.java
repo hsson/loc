8c79a18cda0828ea7d39b82020e19261a8a68556
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-18 10:30:57 +0200

    Adds menu option to exit game

---
+++ core/src/main/java/edu/chl/loc/models/menu/ExitOption.java
@@ -0,0 +1,14 @@
+package edu.chl.loc.models.menu;
+
+/**
+ * @author Alexander Håkansson
+ * @version 1.0.0
+ * @since 2015-05-18
+ */
+public class ExitOption implements IMenuOption {
+    @Override
+    public void choose() {
+        System.out.println("Closing game.");
+        System.exit(0);
+    }
+}
