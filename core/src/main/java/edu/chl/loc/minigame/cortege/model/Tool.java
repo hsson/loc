b4b92851155191a34ccfebdae64443a3573c1a09
Maxim Goretskyy <13maxim13@live.se>
2015-05-21 12:36:42 +0200

    Added equals and hashcode method to Tool

--- core/src/main/java/edu/chl/loc/minigame/cortege/model/Tool.java
+++ core/src/main/java/edu/chl/loc/minigame/cortege/model/Tool.java
@@ -22,4 +22,25 @@ public class Tool extends Rectangle {
     public ToolType getType(){
         return this.type;
     }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == this){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        if(!this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        Tool temp = (Tool)o;
+        return temp.getType() == this.getType() && temp.getX() == this.getX() &&
+                temp.getY() == this.getY();
+    }
+
+    @Override
+    public int hashCode(){
+        return 31*97*getType().hashCode();
+    }
 }
