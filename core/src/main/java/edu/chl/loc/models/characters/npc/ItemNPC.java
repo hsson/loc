445b0b3c7df8e9a9a37e7052b5c0eca42b50dd34
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-05-27 10:50:27 +0200

    Refactors ItemNPC for better modularity

--- core/src/main/java/edu/chl/loc/models/characters/npc/ItemNPC.java
+++ core/src/main/java/edu/chl/loc/models/characters/npc/ItemNPC.java
@@ -14,25 +14,32 @@ import edu.chl.loc.models.utilities.Position2D;
  */
 public class ItemNPC extends AbstractNPC{
     private Inventory inventory;
+    private Inventory playerInventory;
 
 
-    protected ItemNPC(Position2D pos, Direction dir, int id, String name,  Gender gender, Inventory inventory, Dialog dialog) {
+    protected ItemNPC(Position2D pos,
+                      Direction dir,
+                      int id,
+                      String name,
+                      Gender gender,
+                      Inventory inventory,
+                      Dialog dialog,
+                      Inventory playerInventory) {
         super(pos, dir, id, name, gender, dialog);
         this.inventory = inventory.copy();
+        this.playerInventory = playerInventory;
     }
 
     @Override
     public void doAction() {
-
         // Adds all of this NPC's items to the player
         for (AbstractItem item : inventory.getItems()) {
-            GameModel.getPlayer().getInventory().addItem(item);
-            getInventory().removeItem(item); //remove item from NPC
+            playerInventory.addItem(item);
+            inventory.removeItem(item); //remove item from NPC
         }
     }
 
     /**
-     *
      * @return Inventory that this NPC holds
      */
     public Inventory getInventory(){
