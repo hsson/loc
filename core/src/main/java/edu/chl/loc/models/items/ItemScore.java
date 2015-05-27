0fec10695cba44c0651549131eb31eabd3a9aa73
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-05-27 11:44:44 +0200

    Fixes the stats for HEC

--- core/src/main/java/edu/chl/loc/models/items/ItemScore.java
+++ core/src/main/java/edu/chl/loc/models/items/ItemScore.java
@@ -2,6 +2,7 @@ package edu.chl.loc.models.items;
 
 import edu.chl.loc.models.core.GameModel;
 import edu.chl.loc.models.core.IGameModel;
+import edu.chl.loc.models.core.Stats;
 
 /**
  * Created by Maxim on 15-04-14.
@@ -39,7 +40,6 @@ public class ItemScore extends AbstractItem{
     @Override
     public void use(IGameModel state) {
         state.addHec(hec);
-        state.addPlayerStat("Högskolepoäng", 1.0);
         System.out.println("You drank a " + getItemName());
     }
 
